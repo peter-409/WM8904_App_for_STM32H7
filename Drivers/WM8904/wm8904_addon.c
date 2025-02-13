507d2cf35c64580f98ef670f97925c1f79f5bb8f
peter-409 <194226761+peter-409@users.noreply.github.com>
2025-02-13 14:51:16 +0200

    Step 5 - Defined WM8904_InitLoopback()
    This function can be updated with register mappings defined in wm8904_reg.h (see //TODO:)

--- Drivers/WM8904/wm8904_addon.c
+++ Drivers/WM8904/wm8904_addon.c
@@ -21,12 +21,15 @@
  * 
  * 5. Enable left and right output channels
  * 	R14=0b11 (0x3)			# HPL_PGA_ENA=1 HPR_PGA_ENA=1 
- **/ Function to initialize the WM8904 codec in loopback mode
+ * 
+ **/ 
 
-
+// Function to initialize the WM8904 codec in loopback mode
 int32_t WM8904_InitLoopback(WM8904_Object_t *pObj)
 {
     WM8904_Init_t codec_init;
+    int32_t ret;
+    uint16_t reg_value;
 
     // Set the input device to MIC1 and MIC2
     codec_init.InputDevice = WM8904_IN_MIC1_MIC2;
@@ -43,6 +46,60 @@ int32_t WM8904_InitLoopback(WM8904_Object_t *pObj)
     // Set the volume (e.g., 50%)
     codec_init.Volume = 50;
 
+    // Write 0x3 to register 12 (0x0C) to enable left and right input PGAs
+    reg_value = 0x3;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x0C, &reg_value, 2U); // TODO: Replace 0x0C with WM8904_PWR_MANAGEMENT0
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+
+    // Write 0x54 to register 46 (0x2E) to select analogue input configuration for left channel
+    reg_value = 0x54;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x2E, &reg_value, 2U); // TODO: Replace 0x2E with WM8904_ANALOG_LEFT_INPUT1
+    if (ret != WM8904_OK) {
+        return ret;
+    }   
+
+    // Write 0x54 to register 47 (0x2F) to select analogue input configuration for right channel
+    reg_value = 0x54;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x2F, &reg_value, 2U); // TODO: Replace 0x2F with WM8904_ANALOG_RIGHT_INPUT1
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+
+    // Write 0xC to register 61 (0x3D) to enable analog bypass paths for headphone outputs
+    reg_value = 0xC;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x3D, &reg_value, 2U); // TODO: Replace 0x3D with WM8904_ANALOG_OUTPUT12_ZC
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+
+    // Write 0x39 to register 57 (0x39) to set volume level for left channel
+    reg_value = 0x39;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x39, &reg_value, 2U); // TODO: Replace 0x39 with WM8904_ANALOG_OUTPUT1_LEFT
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+    
+    // Write 0x39 to register 58 (0x3A) to set volume level for right channel
+    reg_value = 0x39;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x3A, &reg_value, 2U); // TODO: Replace 0x3A with WM8904_ANALOG_OUTPUT1_RIGHT
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+
+    // Write 0x3 to register 14 (0x0E) to enable left and right output channels
+    reg_value = 0x3;
+    ret = wm8904_write_reg(&pObj->Ctx, 0x0E, &reg_value, 2U); // TODO: Replace 0x0E with WM8904_PWR_MANAGEMENT2
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+    
     // Initialize the codec with the specified parameters
-    return WM8904_Init(pObj, &codec_init);
+    ret = WM8904_Init(pObj, &codec_init);
+    if (ret != WM8904_OK) {
+        return ret;
+    }
+
+    return WM8904_OK;
 }
